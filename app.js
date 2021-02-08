6a0f907c058f8cb6e50dd7f21026312f8bb54daa
Colin Huckestein <cphuckestein@gmail.com>
2021-02-08 13:47:52 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+// name = "Colin Huckestein";
+// var name;
+// console.log(name);
+
+// setName();
+// function setName() {
+//     var name = "Colin Huckestein";
+//     console.log(name);
+// };
+
+// console.log('first text');
+// let avg = findAvg(2, 2);
+// console.log('function result', avg);
+// function findAvg(a, b) {
+//     console.log('function running');
+//     var answer = (a + b) / 2;
+//     return answer;
+// };
+
+// let fruits = ["apple", "orange", "banana"];
+
+// function printFruits() {
+//     let favFruit = fruits[1];
+//     console.log(fruits[0]);
+
+//     printFavFruit();
+
+//     console.log(leastFav);
+
+//     function printFavFruit() {
+//         console.log(favFruit);
+//         let leastFav = fruits[2];
+
+//     };
+// };
+
+// printFruits();
+
+
+myFunc();
+
+function myFunc() {
+    console.log('hello, Colin');
+}
+
+newFunc();
+
+function newFunc() {
+    alert('hello, friend.');
+};
